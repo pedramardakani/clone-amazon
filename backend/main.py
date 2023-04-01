ca869bbb56eb7160f8466ee1c9f39d82e3c5e592
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-04-01 14:09:16 +0330

    backend: set up test environment, add meta-data
    Until now, the OpenAPI docs showed the default FastAPI placeholders.
    
    With this commit, the names and project title are updated to describe
    the project.
    
    Also, now the testing environment is ready.

--- backend/main.py
+++ backend/main.py
@@ -5,9 +5,22 @@
 
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Madagascar",
+    version="0.0.0",
+    description="An e-commerce website API similar to amazon, built on FastAPI, and ArangoDB.",
+    contact={
+        "name": "Pedram Ashofteh-Ardakani",
+        "email": "pedramardakani@pm.me",
+        "url": "https://pedramardakani.wordpress.com"
+    },
+    license_info={
+        "name": "GNU General Public License Version 3.0 or later",
+        "url": "https://www.gnu.org/licenses/"
+    }
+)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!"}
---
+++ backend/test_main.py
@@ -0,0 +1,15 @@
+"""Unit tests.
+
+Tests must be 'sync' (i.e. no async and await in code)."""
+
+from fastapi.testclient import TestClient
+
+from .main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World!"}
