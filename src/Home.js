eb73ad7508ff3d55567933e97898967b44dbfde8
Matin Torkian <matin.torkian.92@gmail.com>
2023-04-14 14:42:09 +0330

    products connected to the basic data

--- src/Home.js
+++ src/Home.js
@@ -2,145 +2,43 @@ import React from "react";
 import "./Home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as Icon from "react-feather";
+import data from "./data.js";
 
 export default function Home() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col">
-          <div className="product-card">
-            <img src={require("./coat.jpg")} class="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Cotton Coat</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <div className="rating-stars">
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
+        {data.products.map((product) => (
+          <div className="col">
+            <div key={product._id} className="product-card">
+              <img
+                src={require(`${product.image}`)}
+                className="card-img-top"
+                alt={product.name}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{product.name}</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <div className="price-tag">{product.price}</div>
+                <div className="rating-stars">
+                  <Icon.Star color="#c9c956" />
+                  <Icon.Star color="#c9c956" />
+                  <Icon.Star color="#c9c956" />
+                  <Icon.Star color="#c9c956" />
+                  <Icon.Star color="#c9c956" />
+                </div>
+                <div className="buy-button">
+                  <a href="/" className="btn btn-primary ">
+                    Buy now
+                  </a>
+                </div>
               </div>
-              <a href="/" class="btn btn-primary">
-                Buy now
-              </a>
             </div>
           </div>
-        </div>
-        <div className="col">
-          <div className="product-card">
-            <img src={require("./coat.jpg")} class="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Cotton Coat</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <div className="rating-stars">
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-              </div>
-              <a href="/" class="btn btn-primary">
-                Buy now
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="product-card">
-            <img src={require("./coat.jpg")} class="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Cotton Coat</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <div className="rating-stars">
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-              </div>
-              <a href="/" class="btn btn-primary">
-                Buy now
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <div className="product-card">
-            <img src={require("./coat.jpg")} class="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Cotton Coat</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <div className="rating-stars">
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-              </div>
-              <a href="/" class="btn btn-primary">
-                Buy now
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="product-card">
-            <img src={require("./coat.jpg")} class="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Cotton Coat</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <div className="rating-stars">
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-              </div>
-              <a href="/" class="btn btn-primary">
-                Buy now
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="product-card">
-            <img src={require("./coat.jpg")} class="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Cotton Coat</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <div className="rating-stars">
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-                <Icon.Star color="#c9c956" />
-              </div>
-              <a href="/" class="btn btn-primary">
-                Buy now
-              </a>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
